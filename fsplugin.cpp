8d97fed1fcb029730cac40855164b62f3a982fcc
BigIskander <BigIskander@gmail.com>
2025-11-21 21:53:11 +0300

    Trying to list MTP devices.

--- fsplugin.cpp
+++ fsplugin.cpp
@@ -23,8 +23,40 @@ int DCPCALL FsInitW(int PluginNr, tProgressProcW pProgressProc, tLogProcW pLogPr
 
 HANDLE DCPCALL FsFindFirstW(WCHAR* Path, WIN32_FIND_DATAW *FindData)
 {
-    LIBMTP_Init();
+    LIBMTP_raw_device_t * rawdevices;
+    int numrawdevices;
+    LIBMTP_error_number_t err;
+    int i;
+
     gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"FsFindFirstW was called... success...");
+
+    LIBMTP_Init();
+    err = LIBMTP_Detect_Raw_Devices(&rawdevices, &numrawdevices);
+    
+    switch(err) {
+        case LIBMTP_ERROR_NO_DEVICE_ATTACHED:
+            gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"No raw devices found.");
+            break;
+        case LIBMTP_ERROR_CONNECTING:
+            gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"Detect: There has been an error connecting. Exiting");
+            break;
+        case LIBMTP_ERROR_MEMORY_ALLOCATION:
+            gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"Detect: Encountered a Memory Allocation Error. Exiting");
+            break;
+        case LIBMTP_ERROR_NONE:
+            {
+                gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"Detect: Yes devices... kinda.. not implemented yet");
+                for (i = 0; i < numrawdevices; i++) {
+                    gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"Device:");
+                    gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) rawdevices[i].device_entry.product);
+                }
+            }
+            break;
+        case LIBMTP_ERROR_GENERAL:
+        default:
+            gLogProc(gPluginNumber, MSGTYPE_CONNECT, (WCHAR*) u"Unknown connection error.");
+    }
+    
     /* not implemented yet */
     return (HANDLE)-1;
 }
